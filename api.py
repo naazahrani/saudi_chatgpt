7e067e822049a36f28d99ffdab96084c9e623925
abdulazizab2 <azaz-11@msn.com>
2023-03-16 23:23:17 +0300

    - adding intent classifier and entities extraction function. - adding response post-processing after previous step

--- api.py
+++ api.py
@@ -1,11 +1,41 @@
 import http.client
 import json
+from utils import classify_intent_extract_entities_parser
 
 conn = http.client.HTTPSConnection("experimental.willow.vectara.io")
-api_key = ""
-customer_id = ""
+customer_id = "2702570306"
+api_key = "zqt_oRXzQgfyo2zWb80GcaNoaLrbSU58480yIJSBrg"
 
-def chatgpt(prompt, api_key, customer_id):
+def classify_intent_extract_entities(message):
+  payload = json.dumps({
+  "model": "gpt-3.5-turbo",
+  "messages": [
+    {
+      "role": "user",
+      "content": f"Classify the intent of the following user's message and extract the entities:\
+        '{message}'\
+          Intent: [Write answer here]\
+          Entities: [Write answer/s here]\
+            If something is unknown or unclear write None in the answer" 
+    }
+    ]
+  })
+  headers = {
+    'Content-Type': 'application/json',
+    'customer-id': customer_id,
+    'x-api-key': api_key
+  }
+  
+  conn.request("POST", "/v1/chat/completions", payload, headers)
+  res = conn.getresponse()
+  data = res.read()
+  print(data.decode("utf-8"))
+
+  ans = json.loads(data)
+  return ans["choices"][0]["message"]["content"]
+
+
+def chatgpt(prompt):
   payload = json.dumps({
   "model": "gpt-3.5-turbo",
   "messages": [
@@ -20,6 +50,7 @@ def chatgpt(prompt, api_key, customer_id):
     'customer-id': customer_id,
     'x-api-key': api_key
   }
+  
   conn.request("POST", "/v1/chat/completions", payload, headers)
   res = conn.getresponse()
   data = res.read()
@@ -29,6 +60,5 @@ def chatgpt(prompt, api_key, customer_id):
   return ans["choices"][0]["message"]["content"]
 
 
-while True:
-  prompt = input()
-  print(chatgpt(prompt, api_key, customer_id))
+# print(classify_intent_extract_entities(" i want to book an appointment"))
+classify_intent_extract_entities_parser(classify_intent_extract_entities(" i want to book an appointment"))
---
+++ utils.py
@@ -0,0 +1,24 @@
+import re
+
+def classify_intent_extract_entities_parser(response):
+    
+    pattern = r"Intent:\s*(.*)"
+    intent_match = re.search(pattern, response)
+    intent = intent_match.group(1)
+    
+    
+    
+    
+    pattern = r"- (\w+):\s*(.+)"
+
+    # use regular expression to extract entity lines
+    entity_lines = re.findall(pattern, response)
+
+    # create a dictionary to store entities
+    entities = {}
+
+    # loop through entity lines and add them to the dictionary
+    for entity_key, entity_value in entity_lines:
+        entities[entity_key] = entity_value
+    
+    return intent, entities
