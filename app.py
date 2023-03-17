542fbc1b2b97ea2b80a7b25f4e39a43c7f9effd7
abdulazizab2 <azaz-11@msn.com>
2023-03-17 22:53:47 +0300

    bug fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('Home.html')
+    return render_template('home.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
